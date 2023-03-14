a4352454b1ec68c3e6014a325f94b9b6937c904a
Poojitha Anchetty <panchetty@healsecurity.com>
2023-03-14 09:59:17 -0500

    main file

---
+++ src/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Contact from './contactus/Contact';
+import Nav from './Nav';
+
+const Main =()=>{
+    return(
+        <>
+        <Nav/>
+        <Contact/>
+        </>
+    )
+}
+
+export default Main;
