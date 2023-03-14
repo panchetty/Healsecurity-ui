6c7717a25a85f2a56485ce13634f8ddb26bc4a37
Poojitha Anchetty <panchetty@healsecurity.com>
2023-03-14 09:49:11 -0500

    contact us page committed

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Nav from './Nav';
+import Contact from './contactus/Contact';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Nav />
+    <Contact />
   </React.StrictMode>
 );
 
