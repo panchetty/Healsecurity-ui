06ede8e3593dd9cb985ec52431b1eef5e689af64
Poojitha Anchetty <panchetty@healsecurity.com>
2023-04-06 10:32:50 -0500

    added few routing implementations

--- src/footer/Routing.js
+++ src/footer/Routing.js
@@ -1,23 +1,60 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {Menu,Download} from 'antd';
+import { BrowserRouter, Routes, Route,Link } from "react-router-dom";
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
+import {UpSquareOutlined} from '@ant-design/icons'
 import Blogs from "../pages/Blogs";
 import Contact from "../pages/Contact";
 import NoPage from "../pages/NoPage";
 
-const Routing = () =>{
+const Routing = () => {
   return (
     <BrowserRouter>
+      <div>
+        <ul>
+          <Menu mode='horizontal'>
+          <li>
+          <UpSquareOutlined/>
+            <Menu.Item key="hi" >
+            <Link to="/">All
+            </Link>
+            </Menu.Item>
+         </li>
+
+          <li>
+          <UpSquareOutlined/>
+          <Menu.Item key="hi">
+            <Link to="/blogs">Latest articles
+            </Link>
+            </Menu.Item>
+          </li>
+          <li>
+          <UpSquareOutlined/>
+          <Menu.Item key="hi ">
+            <Link to="/contact">Applications
+            </Link>
+            </Menu.Item>
+          </li>
+          <li>
+          <UpSquareOutlined/>
+          <Menu.Item key="hi" icon=''>
+            <Link to="/nopage">Hot News
+            </Link>
+            </Menu.Item>
+          </li>
+          </Menu>
+        </ul>
+
+
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path="/blogs" element={<Blogs />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="*" element={<NoPage />} />
       </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/Blogs.js
+++ src/pages/Blogs.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import axios from "axios";
 
 const Blogs = ()=>{
+    const result = axios.get("http://127.0.0.1:8000/articles");
+    console.log("poojitha",result);
     return(
         <>
         <h1>This is blogs page</h1>
